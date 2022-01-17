b20fe7e07d0d952d81852d380244360602177452
anvar-muzaffarli <prof.anvar2013@gmail.com>
2022-01-17 19:10:40 +0400

    front end 10 qrupu üçün yaradılmış yarımçıq repo

---
+++ ders.js
@@ -0,0 +1,3 @@
+const name = "Vasif"
+console.log("Salam " + name + "hsadhhashd")
+console.log(`   ${name}           sahfasfashfuhasugh`)
---
+++ script.js
@@ -0,0 +1,25 @@
+const siyahi = ["paper.jpeg", "das.png","scissors.png"]
+
+//  DOM Document Object Model 
+
+const kagiz = document.getElementById('paper')
+const das = document.getElementById('das')
+const qayci = document.getElementById('qayci')
+
+//komputerin qerari ve netice olan hisse
+const komputerinSecimi = document.getElementById('komputer-secim')
+const neticeniYaz = document.getElementById('netice')
+
+// hadiseler - onclick, onmouseover, onchange
+kagiz.onclick = seciminiEle
+das.onclick = seciminiEle
+qayci.onclick = seciminiEle
+
+function seciminiEle() {
+
+}
+
+function randomSecim(){
+    
+}
+
